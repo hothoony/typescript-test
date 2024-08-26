ffabdffa15ff9fdc8a3745a020bb642a219c5a07
hothoony <hothoony@hanmail.net>
2024-08-26 13:22:18 +0900

    .

---
+++ lesson01/src/script-simple-types.js
@@ -0,0 +1,10 @@
+(function () {
+    var str = 'james';
+    console.log(str);
+    var num = 123;
+    console.log(num);
+    var bool = true;
+    console.log(bool);
+    bool = false;
+    console.log(bool);
+})();
--- lesson01/src/script-simple-types.ts
+++ lesson01/src/script-simple-types.ts
@@ -1,6 +1,14 @@
 (() => {
 
-let firstname: string = 'james';
-console.log(firstname);
+let str: string = 'james';
+console.log(str);
+
+let num: number = 123;
+console.log(num);
+
+let bool: boolean = true;
+console.log(bool);
+bool = false;
+console.log(bool);
 
 })();
