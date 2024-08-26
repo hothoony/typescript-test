da2c37e4e35b5ff884f415acb7d8b7ce425ba26a
hothoony <hothoony@hanmail.net>
2024-08-26 13:33:34 +0900

    .

---
+++ lesson01/src/ts-array.ts
@@ -0,0 +1,12 @@
+(() => {
+
+let names: string[] = ['james', 'paul', 'akira'];
+console.log(names);
+
+let scores: number[] = [80, 90, 100];
+console.log(scores);
+
+let bools: boolean[] = [true, false, true];
+console.log(bools);
+
+})();
---
+++ lesson01/src/ts-object.ts
@@ -0,0 +1,10 @@
+(() => {
+
+    const car: {type: string, mileage: number } = {
+        type: 'van',
+        mileage: 100,
+    };
+
+    console.log('car', car);
+   
+})();
